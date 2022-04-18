5007ac7a8709acc58d74589e859666fb4ea0c54f
swapnil kumar <skumar15@mt.iitr.ac.in>
2022-04-18 23:59:59 +0530

    first commmit

---
+++ FirstApplication/src/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Shubham"
+                + "");
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+}
